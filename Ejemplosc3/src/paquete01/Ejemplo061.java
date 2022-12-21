adc4901e10ee5ff1ba4b1746269365c7bdcbe04c
raul8005 <rimedina@utpl.edu.ec>
2022-12-21 12:20:20 -0500

    Clase 03

---
+++ Ejemplosc3/src/paquete01/Ejemplo061.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Creación de arreglo bidimensionales
+        
+        int[][] arreglo1 = {{10, 41, 40}, {1, 2, 3}};
+        /*
+        10      41      40
+        1       2       3
+        */
+        int suma = 0;
+        int valor;
+        int valores;
+        String cadena = "";
+        for (int fila = 0; fila < arreglo1.length; fila++) {
+            for (int col = 0; col < arreglo1[fila].length; col++) {
+                valor = arreglo1[fila][col];
+                if (fila==col){
+                    suma = suma + valor;
+                    cadena = String.format("%s%d\n", cadena, arreglo1[fila][col]);
+                }
+                
+            }
+            
+            
+        }
+        System.out.printf("La suma de:\n%sEs igual a: %d\n", cadena, suma);
+    }
+    
+}
